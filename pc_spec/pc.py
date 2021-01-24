a1517decb1f1a9c3ca83d7ff8f1d989af1263b0c
Konrad Kocik <konrad.kocik@gmail.com>
2021-01-24 23:24:03 +0100

    Add docs to PC

--- pc_spec/pc.py
+++ pc_spec/pc.py
@@ -1,26 +1,60 @@
 class PC:
+    """ Represents computer build. """
+
     def __init__(self):
         self.__components = {}
 
     @property
-    def components(self):
+    def components(self) -> dict:
+        """
+        Gets component parts of the PC.
+        :return: PC's components
+        """
         return self.__components
 
-    def add_component(self, category, spec=None):
+    def add_component(self, category: str, spec: dict = None):
+        """
+        Adds new component to the PC.
+        If component with given category already exists then nothing will change.
+        :param category: type of component to be added, i.e. 'cpu'
+        :param spec: specification of component to be added, i.e. {'name': 'i7-9700K', 'freq': '4 GHz'};
+                     defaults to None (empty specification, results in empty dict)
+        """
         if not self.__component_exists(category):
             self.__components[category] = spec if spec else {}
 
-    def remove_component(self, category):
+    def remove_component(self, category: str):
+        """
+        Removes component from the PC.
+        If component with given category doesn't exist then nothing will change.
+        :param category: type of component to be removed, i.e. 'cpu'
+        """
         if self.__component_exists(category):
             del self.__components[category]
 
-    def swap_component(self, category, spec=None):
+    def swap_component(self, category: str, spec: dict = None):
+        """
+        Replaces specification of component from given category with new one.
+        If component with given category doesn't exist then nothing will change.
+        :param category: type of component which specification will be swapped, i.e. 'cpu'
+        :param spec: component's specification which will replace old one, i.e. {'name': 'i7-9700K', 'freq': '4 GHz'};
+                     defaults to None (empty specification, results in empty dict)
+        """
         if self.__component_exists(category):
             self.__components[category] = spec if spec else {}
 
-    def update_component(self, category, param_name, param_value):
+    def update_component(self, category: str, param_name: str, param_value: str):
+        """
+        Updates specification of component from given category.
+        If component with given category doesn't exist then nothing will change.
+        If specification's parameter with given name doesn't exist then it will be added with given value.
+        If specification's parameter with given name already exists then it's value will be updated with given one.
+        :param category: type of component which specification will be updated, i.e. 'cpu'
+        :param param_name: name of specification's parameter which will be updated, i.e. 'freq'
+        :param param_value: value of specification's parameter which will replace old one, i.e. '4 GHz'
+        """
         if self.__component_exists(category):
             self.__components[category][param_name] = param_value
 
-    def __component_exists(self, category):
+    def __component_exists(self, category: str) -> bool:
         return category in self.__components.keys()
