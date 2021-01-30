ffe27abd0bfc04c86cc16926b2dcc60acedbc3c1
Konrad Kocik <konrad.kocik@gmail.com>
2021-01-30 19:43:24 +0100

    Add loading store

--- pc_spec/data.py
+++ pc_spec/data.py
@@ -1,6 +1,6 @@
-from json import dump
+from json import dump, load, JSONDecodeError
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 from pc_spec.pc import PC
 from pc_spec.store import Store
@@ -13,10 +13,26 @@ def save_store(store: Store, target_dir: Path):
     :param store: collection of PCs to be saved
     :param target_dir: path to directory where JSON file will be created
     """
-    file_path = Path(target_dir, 'store.json')
+    file_path = Path(target_dir, __get_store_file_name())
     __create_dir_if_necessary(target_dir)
     serializable_pcs = __to_serializable_pcs(store.pcs)
     __save_to_json(serializable_pcs, file_path)
+
+
+def load_store(source_dir: Path) -> Store:
+    """
+    Loads store from JSON file saved in given directory.
+    If given directory doesn't exist then empty store is loaded.
+    If JSON file in given directory doesn't exist or is empty then empty store is loaded.
+    :param source_dir: path to directory which contains store JSON file
+    :return: loaded store
+    """
+    file_path = Path(source_dir, __get_store_file_name())
+    return Store(__get_pcs_from_json_file(file_path)) if file_path.is_file() else Store()
+
+
+def __get_store_file_name():
+    return 'store.json'
 
 
 def __create_dir_if_necessary(dir_path: Path):
@@ -31,3 +47,18 @@ def __to_serializable_pcs(pcs: List[PC]) -> List[Dict[str, Dict[str, Dict[str, s
 def __save_to_json(serializable: List, file_path: Path):
     with open(file_path, 'w') as json_file:
         dump(serializable, json_file)
+
+
+def __get_pcs_from_json_file(file_path: Path) -> List[PC]:
+    with open(file_path, 'r') as json_file:
+        try:
+            return[PC(*__unpack_serialized_pc(serialized_pc)) for serialized_pc in load(json_file)]
+        except JSONDecodeError:
+            return []
+
+
+def __unpack_serialized_pc(serialized_pc: Dict[str, Dict[str, Dict[str, str]]]) -> \
+        Tuple[str, Dict[str, Dict[str, str]]]:
+    name = list(serialized_pc.keys())[0]
+    components = list(serialized_pc.values())[0]
+    return name, components
--- tests/ut/test_data.py
+++ tests/ut/test_data.py
@@ -1,11 +1,11 @@
-from json import load
+from json import load, dump
 from pathlib import Path
 from shutil import rmtree
 from unittest.mock import Mock
 
 from pytest import fixture
 
-from pc_spec.data import save_store
+from pc_spec.data import save_store, load_store
 
 
 @fixture
@@ -16,19 +16,39 @@ def empty_store():
 
 
 @fixture
-def store(empty_store):
+def store(empty_store, pc_1_name, pc_1_components, pc_2_name, pc_2_components):
     pc_1 = Mock()
-    pc_1.name = 'pc_1'
-    pc_1.components = {'cpu': {'name': 'i7-9700K'},
-                       'gpu': {'name': 'RTX 3070'}}
+    pc_1.name = pc_1_name
+    pc_1.components = pc_1_components
 
     pc_2 = Mock()
-    pc_2.name = 'pc_2'
-    pc_2.components = {'mobo': {'name': 'ASRock Z390 EXTREME4',
-                                'format': 'ATX'}}
+    pc_2.name = pc_2_name
+    pc_2.components = pc_2_components
 
     empty_store.pcs = [pc_1, pc_2]
     return empty_store
+
+
+@fixture
+def pc_1_name():
+    return 'pc_1'
+
+
+@fixture
+def pc_1_components():
+    return {'cpu': {'name': 'i7-9700K'},
+            'gpu': {'name': 'RTX 3070'}}
+
+
+@fixture
+def pc_2_name():
+    return 'pc_2'
+
+
+@fixture
+def pc_2_components():
+    return {'mobo': {'name': 'ASRock Z390 EXTREME4',
+                     'format': 'ATX'}}
 
 
 @fixture
@@ -55,6 +75,21 @@ def test_file_path(test_dir_path):
     return Path(test_dir_path, 'store.json')
 
 
+@fixture
+def create_empty_test_file(create_test_dir, test_file_path):
+    if not test_file_path.is_file():
+        test_file_path.touch()
+
+
+@fixture
+def create_test_file(test_file_path, create_empty_test_file, pc_1_name, pc_1_components, pc_2_name, pc_2_components):
+    store = [{pc_1_name: pc_1_components},
+             {pc_2_name: pc_2_components}]
+
+    with open(test_file_path, 'w') as test_file:
+        dump(store, test_file)
+
+
 def test_save_store_when_target_dir_is_not_there_then_it_is_created(
         empty_store, test_dir_path, test_file_path, remove_test_dir):
     save_store(store=empty_store, target_dir=test_dir_path)
@@ -73,6 +108,28 @@ def test_save_store_when_store_not_empty_then_it_is_saved(
                {store.pcs[1].name: store.pcs[1].components}]
     save_store(store=store, target_dir=test_dir_path)
     __assert_json_file_contains(content=content, file_path=test_file_path)
+
+
+def test_load_store_when_file_is_not_there_then_empty_store_is_loaded(test_dir_path):
+    store = load_store(source_dir=test_dir_path)
+    assert store.pcs == []
+
+
+def test_load_store_when_empty_file_is_there_then_empty_store_is_loaded(
+        test_dir_path, create_empty_test_file, remove_test_dir):
+    store = load_store(source_dir=test_dir_path)
+    assert store.pcs == []
+
+
+def test_load_store_when_file_is_there_then_store_is_loaded(
+        test_dir_path, create_test_file, pc_1_name, pc_1_components, pc_2_name, pc_2_components, remove_test_dir):
+    store = load_store(source_dir=test_dir_path)
+    assert len(store.pcs) == 2
+    pc_1, pc_2 = store.pcs
+    assert pc_1.name == pc_1_name
+    assert pc_1.components == pc_1_components
+    assert pc_2.name == pc_2_name
+    assert pc_2.components == pc_2_components
 
 
 def __assert_json_file_contains(content, file_path):
