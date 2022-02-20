e44dfc9d233ed46c6057bcb162bb3a722a352db8
Konrad Kocik <konrad.kocik@gmail.com>
2022-02-20 20:39:10 +0100

    Allow PCSpecApp to add new PCs, components and spec params

--- pc_spec/app/app.py
+++ pc_spec/app/app.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 from kivy.app import App
+from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
-from kivy.config import Config
+from kivy.uix.textinput import TextInput
 
 from pc_spec.data import load_store
 
@@ -19,7 +20,8 @@ class PCSpecApp(App):
         self.title = 'PC Spec'
         self._colors = {'pink': [120, 0, 255, 0.7],
                         'mint': [0, 255, 3, 0.7],
-                        'black': [255, 255, 255, 1]}
+                        'black': [255, 255, 255, 1],
+                        'red': [200, 0, 0, 1]}
 
         self._main_layout = None
         self._buttons_layout = None
@@ -28,14 +30,18 @@ class PCSpecApp(App):
         self._specs_layout = None
 
         self._pcs_buttons = []
+        self._add_pc_button = None
         self._components_buttons = []
+        self._add_component_button = None
         self._specs_buttons = []
+        self._add_spec_button = None
 
         self._store = load_store(source_dir=Path('/home/bajit/repos/pc-spec/pc_spec/app/data'))
         self._pc = None
+        self._component_category = None
         self._component = None
-        self._spec_name = None
-        self._spec_value = None
+        self._param_name = None
+        self._param_value = None
 
     def build(self):
         self._create_layouts()
@@ -83,17 +89,45 @@ class PCSpecApp(App):
 
     def _create_pcs_buttons(self):
         for pc in self._store.pcs:
-            pc_button = Button(text=pc.name.upper(),
-                               background_color=self._colors['mint'],
-                               color=self._colors['black'])
-            pc_button.bind(on_press=self._on_press_pc_button)
-            pc_button.pc_name = pc.name
-            self._pcs_buttons.append(pc_button)
-            self._pcs_layout.add_widget(pc_button)
+            self._create_pc_button(pc_name=pc.name)
 
-    def _on_press_pc_button(self, instance):
-        self._pc = self._store.get_pc(instance.pc_name)
-        self._color_buttons(buttons=self._pcs_buttons, pressed_button=instance)
+        self._add_pc_button = self._create_add_button(target_layout=self._pcs_layout,
+                                                      on_press=self._add_empty_pc)
+
+    def _create_pc_button(self, pc_name):
+        pc_button = Button(text=pc_name.upper(),
+                           background_color=self._colors['mint'],
+                           color=self._colors['black'])
+        pc_button.bind(on_press=self._select_pc)
+        pc_button.pc_name = pc_name
+        self._pcs_buttons.append(pc_button)
+        self._pcs_layout.add_widget(pc_button)
+
+    def _add_empty_pc(self, _):
+        self._pcs_layout.remove_widget(self._add_pc_button)
+        pc_text_input = TextInput(multiline=False)
+        pc_text_input.background_color = self._colors['mint']
+        pc_text_input.bind(on_text_validate=self._save_pc)
+        self._pcs_layout.add_widget(pc_text_input)
+
+    def _save_pc(self, pc_text_input):
+        pc_name = pc_text_input.text
+
+        if self._store.has_pc(name=pc_name):
+            self._show_error(pc_text_input)
+            return
+
+        self._store.add_pc(name=pc_name)
+        self._pcs_layout.remove_widget(pc_text_input)
+        self._create_pc_button(pc_name=pc_name)
+        self._pcs_layout.add_widget(self._add_pc_button)
+
+    def _cancel_adding_pc(self, pc_text_input):
+        pass
+
+    def _select_pc(self, selected):
+        self._pc = self._store.get_pc(selected.pc_name)
+        self._color_buttons(buttons=self._pcs_buttons, pressed_button=selected)
         self._create_components_buttons()
 
     def _create_components_buttons(self):
@@ -103,40 +137,108 @@ class PCSpecApp(App):
         self._specs_buttons.clear()
         self._specs_layout.clear_widgets()
 
-        for component_name in self._pc.components:
-            component_button = Button(text=component_name.upper(),
-                                      background_color=self._colors['mint'],
-                                      color=self._colors['black'])
-            component_button.bind(on_press=self._on_press_component_button)
-            component_button.component_name = component_name
-            self._components_buttons.append(component_button)
-            self._components_layout.add_widget(component_button)
+        for component_category in self._pc.components:
+            self._create_component_button(component_category)
 
-    def _on_press_component_button(self, instance):
-        self._component = self._pc.components[instance.component_name]
-        self._color_buttons(buttons=self._components_buttons, pressed_button=instance)
+        self._add_component_button = self._create_add_button(target_layout=self._components_layout,
+                                                             on_press=self._add_empty_component)
+
+    def _create_component_button(self, component_category):
+        component_button = Button(text=component_category.upper(),
+                                  background_color=self._colors['mint'],
+                                  color=self._colors['black'])
+        component_button.bind(on_press=self._select_component)
+        component_button.component_category = component_category
+        self._components_buttons.append(component_button)
+        self._components_layout.add_widget(component_button)
+
+    def _add_empty_component(self, _):
+        self._components_layout.remove_widget(self._add_component_button)
+        component_text_input = TextInput(multiline=False)
+        component_text_input.background_color = self._colors['mint']
+        component_text_input.bind(on_text_validate=self._save_component)
+        self._components_layout.add_widget(component_text_input)
+
+    def _save_component(self, component_text_input):
+        component_category = component_text_input.text
+
+        if self._pc.has_component(category=component_category):
+            self._show_error(component_text_input)
+            return
+
+        self._pc.add_component(category=component_category)
+        self._components_layout.remove_widget(component_text_input)
+        self._create_component_button(component_category=component_category)
+        self._components_layout.add_widget(self._add_component_button)
+
+    def _select_component(self, selected):
+        self._component_category = selected.component_category
+        self._component = self._pc.components[selected.component_category]
+        self._color_buttons(buttons=self._components_buttons, pressed_button=selected)
         self._create_specs_buttons()
 
     def _create_specs_buttons(self):
         self._specs_buttons.clear()
         self._specs_layout.clear_widgets()
 
-        for spec_name, spec_value in self._component.items():
-            button_text = spec_value if spec_name.lower() == 'name' else f'{spec_name.upper()}: {spec_value}'
-            spec_button = Button(text=button_text,
-                                 background_color=self._colors['mint'],
-                                 color=self._colors['black'])
-            spec_button.bind(on_press=self._on_press_spec_button)
-            spec_button.spec_name = spec_name
-            spec_button.spec_value = spec_value
-            self._specs_buttons.append(spec_button)
-            self._specs_layout.add_widget(spec_button)
+        for param_name, param_value in self._component.items():
+            self._create_spec_button(param_name, param_value)
 
-    def _on_press_spec_button(self, instance):
-        self._spec_name = instance.spec_name
-        self._spec_value = instance.spec_value
-        self._color_buttons(buttons=self._specs_buttons, pressed_button=instance)
+        self._add_spec_button = self._create_add_button(target_layout=self._specs_layout,
+                                                        on_press=self._add_empty_spec)
+
+    def _create_spec_button(self, param_name, param_value):
+        button_text = param_value if param_name.lower() == 'name' else f'{param_name.upper()}: {param_value}'
+        spec_button = Button(text=button_text,
+                             background_color=self._colors['mint'],
+                             color=self._colors['black'])
+        spec_button.bind(on_press=self._select_spec)
+        spec_button.param_name = param_name
+        spec_button.param_value = param_value
+        self._specs_buttons.append(spec_button)
+        self._specs_layout.add_widget(spec_button)
+
+    def _add_empty_spec(self, _):
+        self._specs_layout.remove_widget(self._add_spec_button)
+        spec_text_input = TextInput(multiline=False)
+        spec_text_input.background_color = self._colors['mint']
+        spec_text_input.bind(on_text_validate=self._save_spec)
+        self._specs_layout.add_widget(spec_text_input)
+
+    def _save_spec(self, spec_text_input):
+        if ':' not in spec_text_input.text:
+            self._show_error(spec_text_input)
+            return
+
+        param_name, param_value = spec_text_input.text.split(':')
+        param_name = param_name.rstrip()
+        param_value = param_value.lstrip()
+
+        if self._pc.has_spec_param(category=self._component_category, param_name=param_name):
+            self._show_error(spec_text_input)
+            return
+
+        self._pc.update_component(self._component_category, param_name, param_value)
+        self._specs_layout.remove_widget(spec_text_input)
+        self._create_spec_button(param_name=param_name, param_value=param_value)
+        self._specs_layout.add_widget(self._add_spec_button)
+
+    def _select_spec(self, selected):
+        self._param_name = selected.param_name
+        self._param_value = selected.param_value
+        self._color_buttons(buttons=self._specs_buttons, pressed_button=selected)
 
     def _color_buttons(self, buttons, pressed_button):
         for button in buttons:
             button.background_color = self._colors['pink'] if button is pressed_button else self._colors['mint']
+
+    def _create_add_button(self, target_layout, on_press):
+        add_button = Button(text='+',
+                            background_color=self._colors['mint'],
+                            color=self._colors['black'])
+        add_button.bind(on_press=on_press)
+        target_layout.add_widget(add_button)
+        return add_button
+
+    def _show_error(self, widget):
+        widget.foreground_color = self._colors['red']
